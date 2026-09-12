acb9ad43b4fa1205161cad6ae79a95ccf94e4fec
Biaacosta <beatrizaraujo2604@gmail.com>
2026-09-12 12:59:17 -0300

    Implementação do exercício 10

---
+++ Exercicio10/exercicio10.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 10</title>
+</head>
+<body>
+    <h1>Cálculo do perímetro</h1>
+    <form method="post" action="resposta10.php">
+        <label for="raio">Informe o raio do círculo:</label>
+        <input type="number" id="raio" name="raio">
+        <button type="submit">Enviar</button>
+    </form>
+    
+</body>
+</html>
---
+++ Exercicio10/resposta10.php
@@ -0,0 +1,4 @@
+<?php
+    $raio = $_POST["raio"];
+    $perimetro = $raio * 2 * 3.14;
+    echo "O perímetro do círculo é: $perimetro";
