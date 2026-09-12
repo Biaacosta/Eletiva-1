f0c50c67fc826ecb8f6d9542baf58fdc566990a0
Biaacosta <beatrizaraujo2604@gmail.com>
2026-09-12 20:02:59 -0300

    Implementação do exercício 19

---
+++ Exercicio19/exercicio19.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 19</title>
+</head>
+<body>
+    <h1>Cálculo de velocidade média</h1>
+    <form method="post" action="resposta19.php">
+        <label for="distancia">Informe a distância que deseja percorrer em metros:</label>
+        <input type="number" id="distancia" name="distancia">
+        <label for="tempo">Informe o tempo percorrido em horas:</label>
+        <input type="number" id="tempo" name="tempo">
+        <button type="submit">Enviar</button>
+    </form>
+    
+</body>
+</html>
