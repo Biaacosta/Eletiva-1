111b0bfd7bdd127506c8f13728e51c162a566bdd
Biaacosta <beatrizaraujo2604@gmail.com>
2026-09-19 11:06:05 -0300

    Implementação do exercicio 3

---
+++ Lista3/Exercicio3/exercicio3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 3</title>
+</head>
+<body>
+    <h1>Palavra em maísuclo e minúsculo</h1>
+    <form method="post" action="resposta3.php">
+        <label for="palavra1">Digite a primeira palavra:</label>
+        <input type="text" id="palavra1" name="palavra1">
+        <label for="palavra2">Digite a segunda palavra:</label>
+        <input type="text" id="palavra2" name="palavra2">
+        <button type="submit">Enviar</button>
+    </form>
+    
+</body>
+</html>
