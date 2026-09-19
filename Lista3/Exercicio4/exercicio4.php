a57a4ad61a552b3e90dec87dbeb1dd2688c6b3f8
Biaacosta <beatrizaraujo2604@gmail.com>
2026-09-19 11:15:55 -0300

    Implementação do exercicio 4

---
+++ Lista3/Exercicio4/exercicio4.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 5</title>
+</head>
+<body>
+    <h1>Verificação de data</h1>
+    <form method="post" action="resposta4.php">
+        <label for="dia">Digite o dia:</label>
+        <input type="number" id="dia" name="dia">
+        <label for="mes">Digite o mês:</label>
+        <input type="number" id="mes" name="mes">
+        <label for="ano">Digite o ano:</label>
+        <input type="number" id="ano" name="ano">
+        <button type="submit">Enviar</button>
+    </form>
+    
+</body>
+</html>
