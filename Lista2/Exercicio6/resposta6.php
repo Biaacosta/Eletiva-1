5b6af6751647868a9bc51b22b792a12dc4f7c633
Biaacosta <beatrizaraujo2604@gmail.com>
2026-09-19 10:00:28 -0300

    Implementação do exercicio 6

---
+++ Lista2/Exercicio6/resposta6.php
@@ -0,0 +1,12 @@
+<?php
+    $numero = $_POST["numero"];
+    $i = 1;
+    $soma = 0;
+
+    while ($i <= $numero) {
+        $soma = $soma + $i;
+        $i++;
+    }
+
+    echo "A soma dos números de 1 até $numero é: $soma";
+?>
