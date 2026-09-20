2588b948d31ae19a923c80f42a74c35d24f0826a
Biaacosta <beatrizaraujo2604@gmail.com>
2026-09-20 12:36:35 -0300

    Implementação do exercicio 7

---
+++ Lista3/Exercicio7/exercicio7.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 7</title>
+</head>
+<body>
+    <h1>Inversão de letras</h1>
+    <form method="post" action="resposta7.php">
+        <label for="palavra">Digite uma palavra:</label>
+        <input type="text" id="palavra" name="palavra">
+        <button type="submit">Enviar</button>
+    </form>
+    
+</body>
+</html>
